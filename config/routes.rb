e5daccbd0586b8810f1a4d2f57ce0952c9ee09a4
Rosario Vitale <59125320+rosariovitale022@users.noreply.github.com>
2019-12-22 22:57:15 +0100

    Added articles model and controller

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,13 @@ Rails.application.routes.draw do
   get 'welcome/index'
   root 'welcome#index'
 
+  resources :articles
+  get 'articles/index'
+  get 'articles/show'
+  get 'articles/new'
+  get 'articles/edit'
+  get 'articles/create'
+  get 'articles/update'
+  get 'articles/destroy'
+
 end
