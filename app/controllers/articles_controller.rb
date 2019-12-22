8f616109e7b07599dd2951b4955e4f1a3187e80f
Rosario Vitale <59125320+rosariovitale022@users.noreply.github.com>
2019-12-23 00:22:43 +0100

    Added article creation

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,22 +1,37 @@
 class ArticlesController < ApplicationController
-  def index
-  end
 
-  def show
+  def index
+    @article = Article.all.order("created_at DESC")
   end
 
   def new
+    @article = Article.new
   end
 
   def edit
   end
 
   def create
+    @article = Article.new(article_params)
+
+    if @article.save
+      redirect_to @article
+    else
+      render 'new'
+    end
+  end
+
+  def show
+    @article = Article.find(params[:id])
   end
 
   def update
   end
 
   def destroy
+  end
+
+  def article_params
+    params.require(:article).permit(:title, :content)
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_22_215601) do
+ActiveRecord::Schema.define(version: 2019_12_22_220532) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "posts", force: :cascade do |t|
+  create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "content"
     t.datetime "created_at", precision: 6, null: false
