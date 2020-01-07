8c26a05ce22844c1f37aefdc93cf44b08939cdd3
Rosario <rosario.vitale022@gmail.com>
2020-01-08 00:48:18 +0100

    Fixed databases

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -15,3 +15,6 @@ config.assets.initialize_on_precompile = false
 //
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
+
+require("trix")
+require("@rails/actiontext")
--- db/migrate/20191222220532_create_articles.rb
+++ db/migrate/20191222220532_create_articles.rb
@@ -2,7 +2,7 @@ class CreateArticles < ActiveRecord::Migration[6.0]
   def change
     create_table :articles do |t|
       t.string :title
-      t.rich_text_area :content
+      t.text :content
 
       t.timestamps
     end
